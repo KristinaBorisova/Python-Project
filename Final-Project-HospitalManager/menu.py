3894cd4fdb54a2b783f75ce27de863daa6e1e232
kristinaborisova <kkristinaborisova@gmail.com>
2021-12-12 23:24:12 +0200

    Fix Menu options

--- Final-Project-HospitalManager/menu.py
+++ Final-Project-HospitalManager/menu.py
@@ -9,11 +9,14 @@ Main program's menu is started from here
 class menu:
     def display_menu():
         print("----User Menu----")
-        print("[1] Show list of Doctors")
-        print("[2] Show list of Patients")
-        print("[3] Show list of Visitors")
-        print("[4] Register a new Patient")
+        print("[1] Show Specialists' Weekly Schedule")
+        print("[2] Show list of Specialists")
+        print("[3] Show list of Patients")
+        print("[4] Show list of Visitors")
         print("[5] Register a new Specialist")
+        print("[6] Register a new Patient")
+        print("[7] Register a new Visitor")
+        print("[8] Show average waiting time")
         print("[0] Exit Menu")
         print("----------------")
 
@@ -22,23 +25,30 @@ class menu:
 
     while option != 0:
         if option == 1:
-            print("<<You have chosen to view the list of Specialists Available>>")
-            print("=====Doctors' Shift DICTIONARY=====")
+            print("\n<<You have chosen to see the hospital Specialists' Weekly Schedule>>")
+            print("=====Doctors' Shifts DICTIONARY=====")
             display_dictionaty_conent(doctors_shifts)
-
+            print("===================================\n")
         elif option == 2:
+            print("<<You have chosen to view the list of Specialists>>")
+            display_list_content(specialists_list)
+
+        elif option == 3:
             print("<<You have chosen to view the list of Patients in the waiting room>>")
             display_list_content(patients_list)
 
-        elif option == 3:
-            print("<<You have chosen to view the list of Visitors>>")
-            display_list_content(visitors_list)
         elif option == 4:
-            print("<<You have chosen to see the average waiting time>>")
-
+            print("\n<<You have chosen to view the list of Visitors>>")
+            display_list_content(visitors_list)
         elif option == 5:
-            print("<<You have chosen to add a new specialist/patient/visitor to the list>>")
+            print("\n<<You have chosen to add a new Specialist>>")
 
+        elif option == 6:
+            print("\n<<You have chosen to add a new Patient>>")
+        elif option == 7:
+            print("<<You have chosen to add a new Visitor>>")
+        elif option == 8:
+            print("<<You have chosen to see the Average Waiting Time>>")
         else:
             print("Invalid Optoin. Please try again!")
 
